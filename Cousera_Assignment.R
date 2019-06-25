606bdcf179642d48d4f0394baa4c27aa598f9b17
Iyke Fred <48203770+Fredase@users.noreply.github.com>
2019-06-25 16:03:51 +0100

    Add files via upload

---
+++ Cousera_Assignment.R
@@ -0,0 +1,12 @@
+makeMatrix <- function(x = matrix()){
+      p <- NULL
+      set <- function(y){
+        x <<- y
+        p <<- NULL
+      }
+      
+      get <- function(x)
+      setmatrix <- function(1/x) p <<- 1 / x
+      getInverse <- function() p
+      
+}
